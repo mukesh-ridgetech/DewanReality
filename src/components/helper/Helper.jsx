36f5a39f5228654fc4c524fbec4fc457f80c6af6
mukesh-ridgetech <mukesh@ridgetechcorp.com>
2024-11-29 19:49:22 +0530

    jhggjhgjgh

--- src/components/helper/Helper.jsx
+++ src/components/helper/Helper.jsx
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://localhost:5000"
-export const baseUrl = "https://dewanbackend-1.onrender.com"
+// export const baseUrl = "https://dewanbackend-1.onrender.com"
+export const baseUrl = "https://api.diwan-realty.com";
